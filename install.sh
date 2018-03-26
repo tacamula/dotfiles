fa8253d59ee625c052afbdceb5a69103b4a51b51
Nakamura Takahiro <takahiro.nakamura@gi-no.jp>
2018-03-26 11:29:28 +0900

    move dotfiles under home directory

--- install.sh
+++ install.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-echo "link dotfiles"
-for file in $HOME/dotfiles/.??*
-do
-    [[ "$file" =~ .+/\.git ]] && continue
-    [[ "$file" =~ .+/\.DS_Store ]] && continue
-
-    echo "link $file to $HOME/$(basename $file)"
-    ln -s $file $HOME/$(basename $file)
-done
-
-# TODO: link files unser 'config'
+# TODO: call homebrew_install
 
 echo "set zsh as default shell"
 chsh -s $(which zsh)
@@ -20,3 +10,18 @@ setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+
+echo "-- install tmux plugin manager"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo "-- link dotfiles"
+for file in $PWD/home/*
+do
+    [[ "$file" =~ .+/\.git ]] && continue
+    [[ "$file" =~ .+/\.DS_Store ]] && continue
+
+    echo "link $file to $HOME/$(basename $file)"
+    ln -s $file $HOME/.$(basename $file)
+done
+
+# TODO: link files under 'config'
