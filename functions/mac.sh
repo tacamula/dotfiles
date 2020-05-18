dffa023169635194346e6903d7463a1f8634ed33
na9amura <4410504+na9amura@users.noreply.github.com>
2020-05-18 20:37:32 +0900

    set key repeat setting for VSCode

--- functions/mac.sh
+++ functions/mac.sh
@@ -1,3 +1,9 @@
-defaults write -g InitialKeyRepeat -int 10
-defaults write -g KeyRepeat -int 1
 defaults write com.apple.finder AppleShowAllFiles YES
+
+# key repeats
+## Mac
+defaults write -g InitialKeyRepeat -int 15
+defaults write -g KeyRepeat -int 2
+## VSCode
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
